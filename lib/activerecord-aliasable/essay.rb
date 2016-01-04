9bf946d15e4b0e319bc6f0d7dcafaeded33e590a
Yaroslav Konoplov <yaroslav@inbox.com>
2016-01-04 19:27:05 +0200

    Update essay

--- lib/activerecord-aliasable/essay.rb
+++ lib/activerecord-aliasable/essay.rb
@@ -3,16 +3,20 @@ module Essay
     def aliasable?
       model_class.attributes_marked_as_alias.any?
     end
+
+    serialize do
+      { is_aliasable: aliasable? }
+    end
   end
-  class AttributeRoles
+  class AttributeFeatures
     def alias?
       model_class.attributes_marked_as_alias.include?(attribute_name)
     end
 
-    alias human_id? alias?
-
-    def alias_part?
-
+    serialize do
+      { is_alias: alias? }
     end
+
+    alias human_id? alias?
   end
 end
