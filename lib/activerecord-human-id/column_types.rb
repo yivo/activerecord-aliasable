ced8dffb1a8db455240da8b59ec13f6305def122
Yaroslav Konoplov <yaroslav@inbox.com>
2016-01-13 17:49:28 +0200

    Add column types

---
+++ lib/activerecord-human-id/column_types.rb
@@ -0,0 +1,17 @@
+module HumanID
+  module ColumnTypes
+    def human_id(*args)
+      options         = args.extract_options!.reverse_merge!(null: false, default: '')
+      options[:index] = options[:index] == false ?
+        false :
+        (options[:index] || {}).reverse_merge!(unique: true)
+      args.each { |name| column(name, :string, options) }
+    end
+
+    # Commonly used
+    def alias(*args)
+      args << :alias if args.empty? || args.first.kind_of?(Hash)
+      human_id(*args)
+    end
+  end
+end
